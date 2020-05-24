673b5c8042d0cf680d1426519c8b34162fa41ae7
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-05-24 18:15:22 -0400

    Updated JavaScript file invitation.js

--- js/invitation.js
+++ js/invitation.js
@@ -1,17 +1,17 @@
 //function created to replace the content
 function replaceContent() {
-	//declaring the variables
-	var myRecipientName;
-	var myhostName;
-	
-	//setting the variable to the input field's id named recipientNameInput's value
+	//declaring two variables for both fields: Recipient Name and Host name.
+	var myRecipientName, myhostName;
+
+	//setting the variable to the input field's id named recipientNameInput's value from Recipient Name field
 	myRecipientName = document.getElementById("recipientNameInput").value;
-	myhostName=document.getElementById("hostNameInput").value;
-	
+	// hostNameInput from form field: Host Name
+	myhostName = document.getElementById("hostNameInput").value;
+
 	console.log('Variable myRecipientName: ' + myRecipientName);
 	console.log('variable myhostName: ' + myhostName);
-	
-	//setting the HTML code in the span id recipientNamePlaceholder with the variable 
+
+	//setting the HTML code in the span id recipientNamePlaceholder and hostNamePlaceholder with the two variables, so it can displayed back to HTML
 	document.getElementById("recipientNamePlaceholder").innerHTML = myRecipientName;
 	document.getElementById("hostNamePlaceholder").innerHTML = myhostName;
-} 
+}
