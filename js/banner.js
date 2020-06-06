3ab562a7b59d0fb13ca307c96672415e077bfc65
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-06 16:18:05 -0400

    Updated - Added Index.html Banner Rotation and JS File

---
+++ js/banner.js
@@ -0,0 +1,54 @@
+var images = [];
+        var currentBannerImg = 0;
+        var pause = false;
+    
+        function preloadAllImages() {
+            // get the src of all images in docuemnt and then preload them
+            for(var i = 0; i < images.length; i++) {
+                var img = new Image();
+                img.src = images[i].src;
+            }
+            console.log('Preloading completed!')
+        }
+    
+        function cycleBanner() {
+    
+            // if paused, then don't do anything
+            if(!pause) {
+                // first hide all banner images
+                for(var i = 0; i < images.length; i++) {
+                    images[i].style.display = "none";
+                }
+    
+                // increment the counter to next image
+                currentBannerImg++;
+                if(currentBannerImg == images.length) {
+                    currentBannerImg = 0;
+                }
+                // make current image visible
+                images[currentBannerImg].style.display = "block";
+    
+                console.log('Cycling through banner images')
+            }
+    
+            // call the cycleBanner function after 3 seconds again, to make cycle
+            setTimeout(cycleBanner, 3000);
+        }
+    
+        window.onload = function() {
+            // find all images in document inside bannerImages section
+            images = document.getElementById('bannerImages').getElementsByTagName('img');
+    
+            preloadAllImages();
+            cycleBanner();
+    
+            document.getElementById('bannerImages').onmouseover = function(e) {
+                pause = true;
+                console.log('pausing')
+            };
+            document.getElementById('bannerImages').onmouseout = function(e) {
+                pause = false;
+                console.log('resuming')
+            };
+    
+        };
