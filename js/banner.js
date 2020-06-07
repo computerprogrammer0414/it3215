b8aab71affb9084e6f942d51a27bd9e3bf1b3ce3
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-07 18:51:13 -0400

    Updated html and gallery files - debugged

--- js/banner.js
+++ js/banner.js
@@ -1,7 +1,12 @@
-var images = [];
+/* Jaime Smith
+Last updated on 06/07/2020 */
+/* --------------------------- INITIATE VARIABLES --------------------------- */
+        var images = [];
         var currentBannerImg = 0;
         var pause = false;
     
+/* --------------------- FUNCTION TO PRELOAD ALL IMAGES --------------------- */
+
         function preloadAllImages() {
             // get the src of all images in docuemnt and then preload them
             for(var i = 0; i < images.length; i++) {
@@ -11,44 +16,44 @@ var images = [];
             console.log('Preloading completed!')
         }
     
+/* ------------------------ FUNCTION TO CYCLE IMAGES ------------------------ */
+
         function cycleBanner() {
-    
-            // if paused, then don't do anything
+           // if paused, then don't do anything
             if(!pause) {
-                // first hide all banner images
+                // Hide all banner images
                 for(var i = 0; i < images.length; i++) {
                     images[i].style.display = "none";
                 }
-    
-                // increment the counter to next image
+               // increment the counter to next image
                 currentBannerImg++;
                 if(currentBannerImg == images.length) {
                     currentBannerImg = 0;
                 }
                 // make current image visible
                 images[currentBannerImg].style.display = "block";
-    
                 console.log('Cycling through banner images')
             }
-    
-            // call the cycleBanner function after 3 seconds again, to make cycle
+           // call the cycleBanner function after 3 seconds again, to make cycle
             setTimeout(cycleBanner, 3000);
         }
-    
-        window.onload = function() {
+
+/* ------------------------- WINDOW ONLOAD FUNCTION ------------------------- */
+            window.onload = function() {
             // find all images in document inside bannerImages section
             images = document.getElementById('bannerImages').getElementsByTagName('img');
     
             preloadAllImages();
             cycleBanner();
-    
+
+/* ----------------------- PAUSING CYCLE ON MOUSE OVER ---------------------- */
             document.getElementById('bannerImages').onmouseover = function(e) {
                 pause = true;
                 console.log('pausing')
             };
+/* ------------------------ RESUME CYCLE ON MOUSEOUT ------------------------ */
             document.getElementById('bannerImages').onmouseout = function(e) {
                 pause = false;
                 console.log('resuming')
             };
-    
         };
--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,12 @@
+/* Jaime Smith
+Last updated on 06/06/2020 */
+
 let lastImg = 1; //Set initial thumbnail and preview
+// Get Information from HTML INDEX file
 document.getElementById(0).src = document.getElementById(lastImg).src;
 document.getElementById(lastImg).className = "thumb selected";
 
+// Function to process image by ID
 function preview(img) {
     document.getElementById(lastImg).className = "thumb normal";
     img.className = "thumb selected";
