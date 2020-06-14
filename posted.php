431eb903285860a1c214a010d310ffe1fffc3fd1
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-14 19:00:58 -0400

    Workable PHP for Form Submission

--- posted.php
+++ posted.php
@@ -1,20 +1,17 @@
  		
-  <?php
-  echo "<div style='font-size:18px;color:black;font-family:Verdana;margin:40px;background:#d0d7e2;padding:10px;'>";
-  echo "<h3>POSTED DATA:</h3>";
+<html>
+<body>
 
-  echo "<div style='margin:40px;background-color:#ccc;padding:20px;font-size:18px;'>";
- 
-  echo "==================<br>";
-  foreach ($_POST as $key => $value) {        
-    $output .= $key."---".$value."<br>";
-    echo $key; 
-    echo "--------------------".$value;
-    echo "<br>";         
-  }
-  echo "==================<br>";
-  echo "</div>";
+Welcome <?php echo $_POST["firstname"]; ?><br>
 
-
-?>
+<h1>Your Form was successful Submitted</h1> 
+<h1>Here is the information You've entered:</h1>
+<b>Username: </b> <?php echo $_POST["username"]; ?></br>
+<b>First Name:</b> <?php echo $_POST["firstname"]; ?></br>
+<b>Last Name:</b> <?php echo $_POST["lastname"]; ?></br>
+<b>Email address:</b> <?php echo $_POST["email"]; ?></br>
+<b>Phone Number:</b> <?php echo $_POST["phone"]; ?></br>
+<h1>Thank You!</h1>
+</body>
+</html>
  
